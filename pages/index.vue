3e93f3e9c8b5a33976052981dc7a04dc0ffdddf9
greeng00se <thegreengoose7@gmail.com>
2022-07-07 09:19:56 +0900

    feat: 기본 설정 완료
    - windicss
    - iconify
    - element-plus
    - pinia

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { Icon } from "@iconify/vue"
+import { ElButton } from "element-plus"
+</script>
+
+<template>
+  <div>
+    <el-button>Default</el-button>
+  </div>
+</template>
+
