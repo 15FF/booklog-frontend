32b0ed0bfc8b825924d5436586c77f893efb6ac0
greeng00se <thegreengoose7@gmail.com>
2022-07-11 16:59:35 +0900

    [#1] fix: review 생성 부분도 vuetify로 변경
    Co-authored-by: qilip <qilip@qilip.io>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,19 @@
 <template>
   <v-layout>
     
-    <v-app-bar flat>
-      <v-app-bar-title href="/">BookLog</v-app-bar-title>
-      <v-spacer></v-spacer>
-
+    <v-app-bar class="animate__animated animate__fadeIn" flat>
+      <v-btn variant="text" href="/">BookLog</v-btn>
       <v-btn variant="text" href="/review" >Review</v-btn>
+      <v-spacer></v-spacer>
       <v-btn variant="text" href="/user" icon="mdi-account-outline"></v-btn>
     </v-app-bar>
     
     <v-main>
+      <v-divider></v-divider>
       <slot />
+      
     </v-main>
-
+    
   </v-layout>
 </template>
 <script lang="ts" setup>
---
+++ pages/Review.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="animate__animated animate__fadeIn">
+    <v-container>
+      <v-text-field
+        v-model="book.bookQuery"
+        append-icon="mdi-magnify"
+        filled
+        clear-icon="mdi-close-circle"
+        clearable
+        label="도서명"
+        type="text"
+        @click:clear="book.clear()"
+        @click:append="book.call()"
+        v-on:keyup.enter="book.call()"
+      ></v-text-field>
+    </v-container>
+    <v-container>
+      <v-radio-group
+        :v-model="review.selectedBooks"
+        mandatory
+      >
+      <v-table>
+        <thead>
+          <tr>
+            <th></th>
+            <th class="text-left">도서명</th>
+            <th class="text-left">저자</th>
+            <th class="text-left">출판사</th>
+            <th class="text-left">출판년도</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr
+            v-for="item in book.books"
+            :key="item.isbn"
+          >    
+            
+            <td><v-radio :value="item.isbn"></v-radio></td>
+            <td>{{ item.title }}</td>
+            <td>{{ item.author }}</td>
+            <td>{{ item.publisher }}</td>
+            <td>{{ item.pubdate }}</td>
+          </tr>
+        </tbody>
+      </v-table>
+      </v-radio-group>  
+    </v-container>
+  </div>
+</template>
+
+<script setup>
+import { useBookStore } from '../stores/book';
+import { useReviewStore } from '../stores/review';
+
+const book = useBookStore();
+const review = useReviewStore();
+
+const tableLayout = 'auto';
+</script>
+
+<style scoped>
+</style>
