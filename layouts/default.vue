41e82f320c5140480d6e1b6a96c487479c0f70a8
greeng00se <thegreengoose7@gmail.com>
2022-07-11 15:15:46 +0900

    [#1] fix: header vuetify로 변경

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,40 +1,21 @@
 <template>
-  <el-row>
-    <el-col :span="2"></el-col>
-    <el-col :span="20">
-      <el-menu
-        :default-active="activeIndex"
-        class="el-menu-demo"
-        mode="horizontal"
-        :ellipsis="false"
-        active-text-color="#555555"
-        text-color="#555555"
-      >
-        <el-menu-item index="0">BookLog</el-menu-item>
-        <el-menu-item index="1">Reviews</el-menu-item>
-        <div class="flex-grow" />
-        <el-menu-item index="2">
-          <div><el-icon><User style="font-size:24px;"/></el-icon></div>
-        </el-menu-item>
-      </el-menu>
-    </el-col>
-    <el-col :span="2"></el-col>
-  </el-row>
-  <slot />
+  <v-layout>
+    
+    <v-app-bar flat>
+      <v-app-bar-title href="/">BookLog</v-app-bar-title>
+      <v-spacer></v-spacer>
+
+      <v-btn variant="text" href="/review" >Review</v-btn>
+      <v-btn variant="text" href="/user" icon="mdi-account-outline"></v-btn>
+    </v-app-bar>
+    
+    <v-main>
+      <slot />
+    </v-main>
+
+  </v-layout>
 </template>
-
 <script lang="ts" setup>
-import { 
-  ElMenu,
-  ElMenuItem,
-  ElImage,
-  ElRow,
-  ElCol,
-  ElIcon,
-} from "element-plus"
-import { Icon } from "@iconify/vue";
-
-const activeIndex = ref('1')
 </script>
 
 <style scoped>
