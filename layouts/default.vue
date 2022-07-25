27a794fb4864a0cefbd9100ef4b6d1b624d2ed7f
greeng00se <thegreengoose7@gmail.com>
2022-07-25 14:57:33 +0900

    [#16] feat: 테마 변경 기능 추가
    Co-authored-by: SangMin Shin <qilip@qilip.io>

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,6 +5,7 @@
       <v-btn variant="text" to="/">BookLog</v-btn>
       <v-btn variant="text" to="/new-review">New Review</v-btn>
       <v-spacer></v-spacer>
+      <v-btn @click="toggleTheme" :icon="themeIcon"></v-btn>
       <v-btn variant="text" icon="mdi-account-outline">
         <v-icon>mdi-account-outline</v-icon>
         <v-menu activator="parent">
@@ -30,6 +31,15 @@
   </v-layout>
 </template>
 <script lang="ts" setup>
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+const themeIcon = ref('mdi-weather-sunny');
+
+const toggleTheme = () => {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+  themeIcon.value = themeIcon.value == 'mdi-weather-night' ? 'mdi-weather-sunny' : 'mdi-weather-night';
+}
 </script>
 
 <style scoped>
