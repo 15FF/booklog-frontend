c6d2cba0d42d9f7fe461785a532146ae8a631011
greeng00se <thegreengoose7@gmail.com>
2022-07-25 09:54:52 +0900

    [#14] feat: 로그인 rule 통과시 버튼 활성화 & 토큰 받아오도록 설정

--- pages/user/signin.vue
+++ pages/user/signin.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="animate__animated animate__fadeIn">
+  <div>
     <v-container align="center">
       <v-card max-width="420px">
         <v-card-item>
@@ -8,14 +8,14 @@
           </v-card-title>
         </v-card-item>
         <v-card-item>
-          <v-form>
-            <v-text-field label="아이디"></v-text-field>
-            <v-text-field label="비밀번호" type="password"></v-text-field>
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <v-text-field v-model="username" :rules="[rules_required, rules_username_length]" clearable label="아이디"></v-text-field>
+            <v-text-field v-model="password" :rules="[rules_required, rules_password_length]" clearable label="비밀번호" type="password"></v-text-field>
           </v-form>
         </v-card-item>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="primary">로그인</v-btn>
+          <v-btn :disabled="!valid" @click="signin" color="primary">회원가입</v-btn>
           <v-btn>취소</v-btn>
         </v-card-actions>
       </v-card>
@@ -24,9 +24,31 @@
 </template>
 
 <script setup>
+import { useAuthStore } from '~~/stores/auth';
 
+const username = ref('');
+const password = ref('');
+let valid = ref(false);
+
+async function signin() {
+  const body = {
+    "username": username.value,
+    "password": password.value,
+  };  
+
+  const result = await useFetch('http://localhost:3000/auth/signin', {
+    method: 'POST',
+    body: body
+  });
+  const auth = useAuthStore();  
+  auth.accessToken = result.data.value.accessToken;
+
+  return navigateTo({
+    path: '/',
+  })
+}
+
+const rules_required = value => !!value  || '필수 필드입니다.';
+const rules_username_length = value => value.length >= 4  || '아이디는 4자리 이상이어야 합니다.';
+const rules_password_length = value => value.length >= 8  || '비밀번호는 8자리 이상이어야 합니다.';
 </script>
-
-<style>
-
-</style>
