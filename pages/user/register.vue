3197b025a6472ab67533e8ff39693cf88cf66948
greeng00se <thegreengoose7@gmail.com>
2022-07-25 09:54:21 +0900

    [#14] feat: 회원가입 form rules 통과시 버튼 활성화

--- pages/user/register.vue
+++ pages/user/register.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="animate__animated animate__fadeIn">
+  <div>
     <v-container align="center">
       <v-card max-width="420px">
         <v-card-item>
@@ -8,28 +8,27 @@
           </v-card-title>
         </v-card-item>
         <v-card-item>
-          <v-form>
-            
-            <v-text-field v-model="username" :rules="[rules_required]" clearable label="아이디"></v-text-field>
-            <v-text-field v-model="password" :rules="[rules_required]" clearable label="비밀번호" type="password"></v-text-field>
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <v-text-field v-model="username" :rules="[rules_required, rules_username_length]" clearable label="아이디"></v-text-field>
+            <v-text-field v-model="password" :rules="[rules_required, rules_password_length]" clearable label="비밀번호" type="password"></v-text-field>
             <v-text-field v-model="passwordConfirm" :rules="[rules_required, rules_confirm]" clearable label="비밀번호 확인" type="password"></v-text-field>
           </v-form>
         </v-card-item>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn @click="register" color="primary">회원가입</v-btn>
+          <v-btn :disabled="!valid" @click="register" color="primary">회원가입</v-btn>
           <v-btn>취소</v-btn>
         </v-card-actions>
       </v-card>
     </v-container>
   </div>
 </template>
-
 <script setup>
 
 const username = ref('');
 const password = ref('');
 const passwordConfirm = ref('');
+let valid = ref(false);
 
 async function register() {
   const body = {
@@ -48,9 +47,7 @@ async function register() {
 }
 
 const rules_required = value => !!value  || '필수 필드입니다.';
-const rules_length = value => !!value.length >= 8  || '비밀번호는 8자리 이상이어야 합니다.';
+const rules_username_length = value => value.length >= 4  || '아이디는 4자리 이상이어야 합니다.';
+const rules_password_length = value => value.length >= 8  || '비밀번호는 8자리 이상이어야 합니다.';
 const rules_confirm = value => value == password.value || '비밀번호와 동일해야 합니다.';
 </script>
-
-<style>
-</style>
