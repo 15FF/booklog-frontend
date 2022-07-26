590308451f00d143386dd516871a766afb628668
greeng00se <thegreengoose7@gmail.com>
2022-07-26 17:31:24 +0900

    [#22] feat: 기본 에러 페이지 추가
    - close #22

---
+++ error.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container align="center">
+    <v-card width="480px">
+      <v-card-item>
+        <v-card-title>
+          404 Not Found
+        </v-card-title>
+      </v-card-item>
+      
+      <v-card-item>
+        주소가 잘못되었거나 오류가 발생했습니다.
+      </v-card-item>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn @click="handleError">메인페이지로 이동</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+
+<script setup>
+const props = defineProps({
+  error: Object
+})
+const handleError = () => clearError('/');
+</script>
