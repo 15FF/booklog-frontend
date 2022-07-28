1982705047f6e95e7ca17ab809eb5af6f1f8ce3c
greeng00se <thegreengoose7@gmail.com>
2022-07-28 17:55:09 +0900

    [#24] feat: 메인페이지 기능 추가
    - api 호출하여 독서록 보이게
    - 무한스크롤 설정

--- pages/index.vue
+++ pages/index.vue
@@ -4,14 +4,14 @@
       <v-row dense>
         <v-col v-for="item in reviewlist.reviewlist" :key="item.title" :cols="4">
           <v-card>
-            <v-img class="align-end" height="200" :src="item.image">
+            <v-img class="align-end" height="200" :src="item.bookImage">
               <v-card-title>{{ item.title }}</v-card-title>
-              <v-card-subtitle>{{ item.user }}</v-card-subtitle>
+              <v-card-subtitle>{{ item.username }}</v-card-subtitle>
             </v-img>
             <div class="d-flex mr-4 align-center">
               <v-card-subtitle class="pt-4"> {{ item.book }} </v-card-subtitle>
               <v-spacer></v-spacer>
-              <v-rating :model-value="item.rating" color="primary" dense half-increments readonly size="14"></v-rating>
+              <v-rating :model-value="item.rating/2" color="primary" dense half-increments readonly size="14"></v-rating>
             </div>
             <v-card-text overflow-hidden>
               {{ item.description }}
@@ -25,6 +25,8 @@
           </v-card>
         </v-col>
       </v-row>
+      <v-card v-intersect="getReview"></v-card>
+      
     </v-container>
   </div>
 </template>
@@ -33,6 +35,10 @@
 import { useReviewlistStore } from '~~/stores/reviewlist';
 
 const reviewlist = useReviewlistStore();
+
+const getReview = async () => {
+  await reviewlist.getReviewList();
+}
 </script>
 
 <style>
