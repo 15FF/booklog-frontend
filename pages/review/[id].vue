ae6be39f3bb1a2ab69c0ef0620fa4bc1d75c3fb5
greeng00se <thegreengoose7@gmail.com>
2022-07-29 21:57:20 +0900

    [#31] feat: 독서록 페이지 임시 생성

---
+++ pages/review/[id].vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-container>
+      <v-card>
+        <v-card-title>
+          {{ $route.params.id }}
+        </v-card-title>
+      </v-card>
+    </v-container>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
