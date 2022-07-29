3afcca3e70b05490602c75d1a6331353780af07e
greeng00se <thegreengoose7@gmail.com>
2022-07-29 21:00:26 +0900

    [#31] feat: BookStore 책 목록 API 호출하도록 변경
    - API 호출하는 메서드의 반환 값은 가져온 책의 개수

--- stores/book.ts
+++ stores/book.ts
@@ -1,50 +1,24 @@
 import { defineStore } from "pinia";
 
 export const useBookStore = defineStore('book', () => {
-  let bookQuery = ref('');
-  let books = ref([]);
+  const bookQuery = ref('');
+  const bookList = ref([]);
 
   function clear() {
-    bookQuery = ref('');
+    bookQuery.value = '';
   }
 
-  function call() {
-    console.log(bookQuery.value);
-    console.log(books.value);
-    books.value = [];
-    const result = {
-      "count": 3,
-      "items": [
-        {
-          "title": "김케장 단편선",
-          "image": "https://bookthumb-phinf.pstatic.net/cover/111/705/11170597.jpg?type=m1&udate=20190813",
-          "author": "김케장",
-          "publisher": "나무야미안해",
-          "pubdate": "2016-10-10",
-          "isbn": "9791195882601"
-        },
-        {
-          "title": "신춘문예와 무관한 시집 (2019)",
-          "image": "https://bookthumb-phinf.pstatic.net/cover/145/684/14568446.jpg?type=m1&udate=20190406",
-          "author": "장지원, 연호, 김컴퓨터, 김현체, 김민수",
-          "publisher": "나무야미안해",
-          "pubdate": "2019-02-02",
-          "isbn": "9791189874056"
-        },
-        {
-          "title": "신춘문예와 무관한 시집 (2019)",
-          "image": "https://bookthumb-phinf.pstatic.net/cover/145/628/14562825.jpg?type=m1&udate=20191129",
-          "author": "장지원",
-          "publisher": "나무야미안해",
-          "pubdate": "2019-02-02",
-          "isbn": "9791189474058"
-        }
-      ]
-    }
+  async function getBookList() {
+    bookList.value = [];
 
-    result.items.forEach((book) => {
-      books.value.push(book);
-    })
+    const { data } = await useFetch(`/api/books?bookQuery=${bookQuery.value}`);
+
+    data.value['items'].forEach((book) => {
+      bookList.value.push(book);
+    });
+
+    return data.value['count'];
   }
-  return { bookQuery, clear, call, books };
+
+  return { bookQuery, bookList, clear, getBookList };
 })
