84e4eb53ecb244c66c5ae0e29502dde251a45649
greeng00se <thegreengoose7@gmail.com>
2022-07-29 21:59:11 +0900

    [#31] feat: 독서록 저장
    - store: 변수 기본값 설정 및 독서록 저장 메서드 생성
    - 뷰: 실패시 alert창 뜨도록 설정

--- pages/new-review/write.vue
+++ pages/new-review/write.vue
@@ -1,29 +1,52 @@
 <template>
   <div>
-    <v-container>
+    <v-container align="center">
       <v-form>
-        <v-text-field v-model="review.selectedBooks.title" label="도서명" disabled></v-text-field>
-        <v-text-field v-model="review.title" label="독서록 제목"></v-text-field>
+        <v-text-field v-model="reviewStore.selectedBooks.title" label="도서명" disabled></v-text-field>
+        <v-text-field v-model="reviewStore.title" label="독서록 제목"></v-text-field>
         <div class="d-flex align-center">
-          <v-rating v-model="review.rating" hover half-increments size="small"></v-rating>
-          <span class="ml-2"> 별점 : {{ review.rating }} </span>
+          <v-rating v-model="reviewStore.rating" hover half-increments size="small"></v-rating>
+          <span class="ml-2"> 별점 : {{ reviewStore.rating }} </span>
         </div>
-        <v-switch v-model="review.status" color="orange" label="독서록을 공개합니다"></v-switch>
-        <v-textarea v-model="review.dsecription" label="독서록 내용" auto-grow></v-textarea>
+        <v-switch v-model="reviewStore.status" true-value="PUBLIC" false-value="PRIVATE" color="orange" label="독서록을 공개합니다"></v-switch>
+        <v-textarea v-model="reviewStore.dsecription" label="독서록 내용" auto-grow></v-textarea>
       </v-form>
       <div class="d-flex justify-end">
-        <v-btn color="primary" size="large" to="/new-review/write" :disabled="!review.selectedBooks">
+        <v-btn color="primary" size="large" @click="saveReview" :disabled="!reviewStore.selectedBooks">
           등록
           <v-icon class="ml-3">mdi-arrow-right</v-icon>
         </v-btn>
       </div>
+    <v-snackbar v-model="saveReviewAlert" location="bottom">독서록 작성에 실패했습니다.
+      <template v-slot:actions>
+        <v-btn color="red" variant="text" @click="saveReviewAlert = false">
+          닫기
+        </v-btn>
+      </template>
+    </v-snackbar>
     </v-container>
   </div>
 </template>
 
 <script setup>
 import { useReviewStore } from '~~/stores/review';
-const review = useReviewStore();
+const reviewStore = useReviewStore();
+const saveReviewAlert = ref(false);
+
+async function saveReview() {
+  const { data, error } = await reviewStore.saveReview();
+
+  if (error.value) {
+    saveReviewAlert.value = true;
+    return;
+  }
+
+  console.log(data.value);
+
+  return navigateTo({
+    path: '/review/' + data.value,
+  })
+}
 </script>
 
 <style>
